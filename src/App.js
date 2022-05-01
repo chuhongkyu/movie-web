6fccfd703b6f92f14f23d081fb3910a3fd7b203c
Mr.chu <chuhongkyu@gmail.com>
2022-05-01 12:35:37 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import {useState, useEffect} from "react";
 
 function App() {
-  return (
-    <div className="App">
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://yts.mx/api/v2/list_movies.json")
+        .then((response)=> response.json())
+        .then((json)=> console.log(json))
+  }, []);
+
+  return(
+    <div>
+      {loading ? <h1>Loading...</h1>: null }
     </div>
-  );
+  )
 }
 
 export default App;
